a586a45939357e843b47ae2394dd2ce629fa4cf5
Juan Alderete <juan41285@gmail.com>
2016-07-03 22:20:26 -0300

    Fix server update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
 
   // /* @ngInject */
   angular
-    .module('portal', ['ngRoute','portal.controllers', 'portal.services', 'portal.filters','ngSanitize','ui.bootstrap','ngScrollable','angularUtils.directives.dirPagination','720kb.socialshare'])
+    .module('portal', ['ngRoute','portal.controllers', 'portal.services','portal.templates', 'portal.filters','ngSanitize','ui.bootstrap','ngScrollable','angularUtils.directives.dirPagination','720kb.socialshare'])
     .config(config);
 
 
