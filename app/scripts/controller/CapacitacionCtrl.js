def61b73babae9c052b50192262505e66d3994a5
System Administrator <root@Mac-mini-de-Administrador.local>
2016-09-07 12:19:21 -0300

    nuevo array de ofertas

--- app/scripts/controller/CapacitacionCtrl.js
+++ app/scripts/controller/CapacitacionCtrl.js
@@ -6,16 +6,32 @@
     .module('portal.controllers')
     .controller('CapacitacionCtrl', CapacitacionCtrl);
 
-    function CapacitacionCtrl($scope, $routeParams, Capacitaciones, ComisionesCapa, SDetalleCom){
+    function CapacitacionCtrl($scope,$rootScope, $routeParams, Capacitaciones, ComisionesCapa, SDetalleCom){
 
     	$scope.sData = {};
   		$scope.mostrar = false;
-
+      $scope.capacitacion = [];
      	// Capacitaciones es un servicio que ejecuta una api /capanew que devuelve 
      	// un arreglo con todas las capacitaciones del segundo semestre de 2016
     	$scope.mostrarCapa = {};
+       $scope.listarCom = {};
       $scope.mostrarCapa =  Capacitaciones.query({});
-		  console.log($scope.mostrarCapa);	
+		  $scope.mostrarCapa.$promise.then(function(res){
+        console.log('res', res);
+        var total = res.length;
+        for (var i = 0; i <= total; i++) {
+             
+       var idc = res[i].id;
+   
+      $scope.listarCom = ComisionesCapa.query({id: idc});
+      $scope.capacitacion.push({capa:res[i], datos:$scope.listarCom, activo: true, collapse: false, id: res[i].id});
+        // console.log('nuevo array', $scope.capacitacion);
+
+        }
+ 
+      }); 
+
+      console.log('nuevo array', $scope.capacitacion);
 
       // DetalleCom es un servicio que ejecuta una api /detalleCom que devuelve 
       // todos los datos de una comision
@@ -25,7 +41,7 @@
       $scope.mostrarCom = SDetalleCom.query({id: $routeParams.idComision});
       console.log($scope.mostrarCom);  
 
-
+     $scope.capaVar = true;
 		$scope.funcionMostrarCom = function(pg_id){
 			$scope.mostrar = true;
 			$scope['capaVar'+pg_id] = !$scope['capaVar'+pg_id];
@@ -36,7 +52,6 @@
 
 
 		}
-
 
 
     }
